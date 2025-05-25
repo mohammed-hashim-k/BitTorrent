0e76f1458ad601c0a87fcbfc0fde8c467ed7451b
mohammed-hashim-k <callistopan@gmail.com>
2025-05-25 10:53:49 +0530

    Adds BEncoding for Byte Array, List, Dictionary

--- BEncoding.cs
+++ BEncoding.cs
@@ -16,6 +16,7 @@ namespace BitTorrent{
         private static byte NumberEnd = System.Text.Encoding.UTF8.GetBytes("e")[0]; // 101
         private static byte ByteArrayDivider = System.Text.Encoding.UTF8.GetBytes(":")[0]; // 58
 
+        # region Decoding
         public static object Decode(byte[] bytes){
             IEnumerator<byte> enumerator = ((IEnumerable<byte>) bytes).GetEnumerator();
             enumerator.MoveNext();
@@ -57,6 +58,79 @@ namespace BitTorrent{
             string numAsString = System.Text.Encoding.UTF8.GetString(bytes.ToArray());
             return Int64.Parse(numAsString);
         }
+
+        // Decode byte array
+        public static byte[] DecodeByteArray(IEnumerator<byte> enumerator){
+            List<byte> lengthBytes = new List<byte>();
+
+            // Scan until a divider
+            do{
+                if(enumerator.Current == ByteArrayDivider){
+                    break;
+                }
+                lengthBytes.Add(enumerator.Current);
+            }
+            while ( enumerator.MoveNext());
+
+            string lengthString  = System.Text.Encoding.UTF8.GetString(lengthBytes.ToArray());
+            int length;
+
+            if (!Int32.TryParse(lengthString, out length)){
+                throw new Exception("unable to parse the length of the array");
+            }
+
+            // read actual byte array
+            byte[] bytes = new byte[length];
+
+            for (int i = 0 ; i < length ; i ++){
+                enumerator.MoveNext();
+                bytes[i] = enumerator.Current;
+            }
+            return bytes;
+        }
+
+        private static DecodeList(IEnumerator<byte> enumerator){
+            List<object> list = new List<object>();
+            // keep pulling objects until we hit the end flag
+            while (enumerator.MoveNext()){
+                if(enumerator.Current == ListEnd){
+                    break;
+                }
+                list.Add(DecodeNextObject(enumerator));
+            }
+            return list;
+        }
+
+        // Decode dictionary
+        private static DecodeDictionary(IEnumerator<byte> enumerator){
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            List<string> keys = new List<string>();
+
+            // keep pulling objects until we hit the end flag
+            while (enumerator.MoveNext()){
+                if(enumerator.Current == DictionaryEnd){
+                    break;
+                }
+                // read key
+                byte[] keyBytes = DecodeByteArray(enumerator);
+                string key = System.Text.Encoding.UTF8.GetString(keyBytes);
+                // read value
+                object value = DecodeNextObject(enumerator);
+                keys.Add(key);
+                dict.Add(key, value);
+            }
+
+            // verify the incoming keys are sorted correctly
+            var sortedKeys = keys.OrderBy(x => BitConverter.ToString(System.Text.Encoding.UTF8.GetBytes(x))); // convert to byte array for comparison
+            if (!keys.SequenceEqual(sortedKeys)){
+                throw new Exception("Dictionary keys are not sorted correctly");
+            }
+            return dict;
+        }
+        # endregion
+
+        # region Encoding
+
 
 
 
